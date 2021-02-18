8efb18e63c2169a7d7d315ffe18558bdf85e4c84
Chana Becker <cgoldzm@gmail.com>
2021-02-18 17:23:06 -0500

    toggle working

--- React0511AddRemoveNumbers/ClientApp/src/NumberRow.js
+++ React0511AddRemoveNumbers/ClientApp/src/NumberRow.js
@@ -3,25 +3,22 @@ import React from 'react';
 class NumberRow extends React.Component {
 
     addOrRemove = action => {
-         //const { onClickAddNumber, onClickRemoveNumber } = this.props;
-         return  action === 'Remove' ? 'onClickRemoveNumber' : 'onClickAddNumber' ;
-        //if (action === 'Remove') {
-        //    return onClickRemoveNumber;
-        //}
-        //return onClickAddNumber;
-    }
+        const { onClickAddNumber, onClickRemoveNumber } = this.props;
+        return action === 'Remove' ? onClickRemoveNumber : onClickAddNumber;
+     }
 
     render() {
-        const { number, nextAction } = this.props.number;//each number contains: number, nextAction so we are destructuring the  prop (pull the number and nextAction within the number prop)
-        const { onClickAddNumber, onClickRemoveNumber } = this.props;
+        const { number, nextAction } = this.props.number;
+        //const { onClickAddNumber, onClickRemoveNumber } = this.props;
 
         { console.log(nextAction) }
-        //{ console.log(this.addOrRemove(nextAction) )}
+        { console.log(this.addOrRemove(nextAction) )}
+
         return (                                           
             <tr>
                 <td>{number}</td>
-                <td><button className="btn btn-primary" onClick={onClickAddNumber}>{nextAction}</button>  </td>
-                {/*<td><button className="btn btn-primary" onClick={this.addOrRemove(nextAction) }>{nextAction}</button>  </td>*/}
+                {/*  <td><button className="btn btn-primary" onClick={onClickAddNumber}>{nextAction}</button>  </td>*/}
+                <td><button className="btn btn-primary" onClick={this.addOrRemove(nextAction) }>{nextAction}</button>  </td>
 
              </tr>
             )
--- React0511AddRemoveNumbers/ClientApp/src/NumberTable.js
+++ React0511AddRemoveNumbers/ClientApp/src/NumberTable.js
@@ -1,6 +1,5 @@
 
 import React from 'react';
-//import NumberForm from './NumberForm';
 import NumberRow from './NumberRow';
 
 
@@ -21,49 +20,39 @@ class NumberTable extends React.Component {
 
     onClickNewNumber = () => {
 
-       const newNum = {
-        number: this.getRandomNumber(1, 1000),
-        nextAction: 'Add'
-        }
+        const newNum ={
+            number: this.getRandomNumber(1, 1000),
+            nextAction: 'Add'
+         }
         const numbersCopy = [...this.state.numbers, newNum];
-        console.log('numbersCopy array:');
-        console.log(numbersCopy);
-
         this.setState({ numbers: numbersCopy });
-        console.log('numbers array:');
-        console.log(this.state.numbers);
-    }
+      }
 
+    //modify the "nextAction" of the number in the main list of numbers from "Add" to "Remove"
     updateAction = (num, newAction) => {
-
-       //modify the "nextAction" of the number in the main list of numbers from "Add" to "Remove"
-        console.log('numbers array in updateAction:')
-        console.log(this.state.numbers);
-
+        
         const numberCopy = [...this.state.numbers];
-        console.log(numberCopy);
-        const index = numberCopy.findIndex(n => n.number === num);//find the location in the array of the number being updated
-        console.log(`index: ${index} for ${num}`);
+        const index = numberCopy.findIndex(n => n.number === num);
         numberCopy[index] = { number: num, nextAction: newAction }; //set the action (used to toggle add/remove button)
         this.setState({ numbers: numberCopy });
      }
 
     onClickAddNumber = num => {
-        const numberList = [...this.state.numbersAdded, num] //add the selected number to the list
+
+        const numberList = [...this.state.numbersAdded, num] 
         console.log(numberList);
         this.setState({ numbersAdded: numberList } );
-        this.updateAction(num, 'Remove');//update the action in the main numbers list 
+        this.updateAction(num, 'Remove');
      }
 
-    onClickRemoveNumber = num =>{
-        const numberList = [...this.state.numbersAdded].filter(n => n.number  !== num);
+    onClickRemoveNumber = num => {
+        const numberList = [...this.state.numbersAdded].filter(n => n !== num);
         this.setState({numbersAdded: numberList });
-        this.updateAction(num, 'Add');//update the action in the main numbers list 
+        this.updateAction(num, 'Add');
     }
 
     render() {
         return (
-
 
             <div className="container" style={{ marginTop: 60 }}>
                 <button className="btn btn-success" onClick={this.onClickNewNumber}>New Number</button>
@@ -90,13 +79,9 @@ class NumberTable extends React.Component {
                 <div className="row jumbitron">
                     
                     <ul>
-                        {console.log('numbersAdded in <li>')}
-                        {console.log(this.state.numbersAdded)}
-
                         {this.state.numbersAdded.map((n, i) =>  <li key={i}>{n}</li>)}
                         </ul>             
                      </div> 
-
                                      
                  </div>
 
